58381bd914d7b02bb768515cfd389822b292c33e
Andre Chalhoub <andrechalhoub@Andres-MacBook-Pro.local>
2024-04-15 00:52:28 -0400

    Attempting to set the value of the post's content when the post is not undefined

--- src/components/post-form/PostForm.tsx
+++ src/components/post-form/PostForm.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../Button";
 import Input from "../Input";
@@ -27,23 +27,15 @@ export default function PostForm({post}:{post: Models.Document | undefined}){
     const {pathname} = useLocation()
     const navigate = useNavigate()
     const userData = useSelector((state: RootState) => state.auth.userData)
-    const [postContent, setPostContent] = useState(post?.content)
 
     useEffect(() => {
-        console.log("render form")
         if(post){
-            setPostContent(getValues("content"))
-            console.log(getValues("content"))
             setValue("title", post.title)
-            setValue("content", post.content)
             setValue("status", post.status)
             setValue("slug", post.$id)
         }
-    }, [setValue, getValues, post])
-
-    useEffect(() => {
-        setValue("content", post?.content)
-    }, [setValue, post])
+        console.log(post)
+    }, [setValue, getValues, post, ])
 
     const submit = async(data: postItem) => {
         if (post) {
@@ -100,65 +92,66 @@ export default function PostForm({post}:{post: Models.Document | undefined}){
             </Container>
         )
     }
-
-    return (
-        <form onSubmit={handleSubmit(submit)}
-        className="flex flex-wrap"
-        >
-            <p className="text-lg w-full pb-5">Fields marked with * are required</p>
-            <div className="w-2/3 px-2">
-                <Input
-                label="Title *"
-                placeholder="Title"
-                className="mb-4"
-                {...register("title", {required: {value:true, message:"Title is required"}})}>
-                {errors.title && <p className="text-red-500">{errors.title.message}</p>}
-                </Input>
-                <Input
-                label="Slug *"
-                placeholder="Slug"
-                className="mb-4"
-                readOnly={post ? true : false}
-                {...register("slug", {required: {value:true, message:"Slug is required"}, maxLength: {value:36, message:"Max length of slug reached"}})}
-                onInput={(e) => {
-                    setValue("slug", slugTransform(e.currentTarget.value), {shouldValidate: true})
-                }}>
-                    {errors.slug && <p className="text-red-500">{errors.slug.message}</p>}
-                </Input>
-                <RealTimeEditor
-                label="Content *"
-                name="content"
-                control={control}
-                defaultValue={postContent}>
-                    {errors.content && <p className="text-red-500">{errors.content.message}</p>}
-                </RealTimeEditor>
+    if (post !== undefined) {
+        setValue("content", post.content)
+        return (
+            <form onSubmit={handleSubmit(submit)}
+            className="flex flex-wrap"
+            >
+                <p className="text-lg w-full pb-5">Fields marked with * are required</p>
+                <div className="w-2/3 px-2">
+                    <Input
+                    label="Title *"
+                    placeholder="Title"
+                    className="mb-4"
+                    {...register("title", {required: {value:true, message:"Title is required"}})}>
+                    {errors.title && <p className="text-red-500">{errors.title.message}</p>}
+                    </Input>
+                    <Input
+                    label="Slug *"
+                    placeholder="Slug"
+                    className="mb-4"
+                    readOnly={post ? true : false}
+                    {...register("slug", {required: {value:true, message:"Slug is required"}, maxLength: {value:36, message:"Max length of slug reached"}})}
+                    onInput={(e) => {
+                        setValue("slug", slugTransform(e.currentTarget.value), {shouldValidate: true})
+                    }}>
+                        {errors.slug && <p className="text-red-500">{errors.slug.message}</p>}
+                    </Input>
+                    <RealTimeEditor
+                    label="Content *"
+                    name="content"
+                    control={control}
+                    defaultValue={getValues("content")}>
+                        {errors.content && <p className="text-red-500">{errors.content.message}</p>}
+                    </RealTimeEditor>
+                </div>
+                <div className="w-1/3 px-2">
+                    <Input
+                    label="Featured Image * "
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg"
+                    {...register("image", {required: {value: !post, message:"Featured Image is required"}})}>
+                        {errors.image && <p className="text-red-500">{errors.image.message}</p>}
+                    </Input>
+                    {post && (
+                        <div className="w-full mb-4">
+                            <img src={appwriteService.getFilePreview(post.featuredImage)} alt={post.title} className="rounded-lg"/>
+                        </div>
+                    )}
+                    <SelectRef
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    required
+                    {...register("status", {required: true})}
+                    />
+                    <Button type="submit" bgColour={post ? "bg-green-500" : "bg-blue-500"} className="w-full">
+                        {post ? "Update" : "Submit"}
+                    </Button>
             </div>
-            <div className="w-1/3 px-2">
-                <Input
-                label="Featured Image * "
-                type="file"
-                className="mb-4"
-                accept="image/png, image/jpg, image/jpeg"
-                {...register("image", {required: {value: !post, message:"Featured Image is required"}})}>
-                    {errors.image && <p className="text-red-500">{errors.image.message}</p>}
-                </Input>
-                {post && (
-                    <div className="w-full mb-4">
-                        <img src={appwriteService.getFilePreview(post.featuredImage)} alt={post.title} className="rounded-lg"/>
-                    </div>
-                )}
-                <SelectRef
-                options={["active", "inactive"]}
-                label="Status"
-                className="mb-4"
-                required
-                {...register("status", {required: true})}
-                />
-                <Button type="submit" bgColour={post ? "bg-green-500" : "bg-blue-500"} className="w-full">
-                    {post ? "Update" : "Submit"}
-                </Button>
-            </div>
-
         </form>
-    )
+        )
+    }
 }
